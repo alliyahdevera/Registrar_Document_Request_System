1cad01adf0c540653d65caecac955667ab86013f
SolisSophiaCassandra <c1395-24@itmlyceumalabang.onmicrosoft.com>
2026-09-16 10:38:22 +0800

    .

--- Registrar Document Request System/My Project/Application.Designer.vb
+++ Registrar Document Request System/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Registrar_Document_Request_System.frmRequestList
+            Me.MainForm = Global.Registrar_Document_Request_System.frmLogin
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
